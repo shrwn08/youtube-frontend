be1b7784cc96a69f12ff3778c5d590eb88defe80
Shrawan <shrwnkumar08@gmail.com>
2025-11-17 21:38:45 +0530

    home component updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllVideos } from "../Redux/slices/videoSlice";
 import VideoItems from "../components/VideoItems";
 
 function Home() {
+  const dispatch = useDispatch();
+  const { videos, isLoading, isError } = useSelector((state) => state.video);
+
+  useEffect(() => {
+    dispatch(getAllVideos());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="w-full min-h-screen flex justify-center items-center">
+        <p className="text-xl">Loading videos...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="w-full min-h-screen flex justify-center items-center">
+        <p className="text-xl text-red-500">Failed to load videos</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="">
-      <VideoItems />
+    <div className="w-full">
+      <VideoItems videos={videos} />
     </div>
   );
 }
