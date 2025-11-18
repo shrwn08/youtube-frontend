bec71510a8e85ffdd0bc35b4eb0020776740fa0d
Shrawan <shrwnkumar08@gmail.com>
2025-11-18 14:05:37 +0530

    VideoGridSkeleton component added

--- src/components/skeletons/VideoGridSkeleton.jsx
+++ src/components/skeletons/VideoGridSkeleton.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import VideoCardSkeleton from "./VideoCardSkeleton";
 
-const VideoGridSkeleton=() => {
+const VideoGridSkeleton = ({ count = 12 }) => {
   return (
-    <div>VideoGridSkeleton</div>
-  )
-}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 p-6">
+      {Array.from({ length: count }).map((_, index) => (
+        <VideoCardSkeleton key={index} />
+      ))}
+    </div>
+  );
+};
 
 export default VideoGridSkeleton;
