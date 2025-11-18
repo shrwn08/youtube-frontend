6b9050767de768561ad3b55fc31550f97e4ef9df
Shrawan <shrwnkumar08@gmail.com>
2025-11-18 14:09:10 +0530

    CommentSkeleton

--- src/components/skeletons/CommentSkeleton.jsx
+++ src/components/skeletons/CommentSkeleton.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
 const CommentSkeleton = () => {
   return (
-    <div>CommentSkeleton</div>
-  )
-}
+    <div className="flex gap-3 py-4 animate-pulse">
+      {/* Avatar */}
+      <div className="w-10 h-10 rounded-full bg-gray-300 flex-shrink-0"></div>
 
-export default CommentSkeleton
+      {/* Comment content */}
+      <div className="flex-1 space-y-2">
+        <div className="h-4 bg-gray-300 rounded w-32"></div>
+        <div className="space-y-2">
+          <div className="h-3 bg-gray-300 rounded w-full"></div>
+          <div className="h-3 bg-gray-300 rounded w-4/5"></div>
+        </div>
+        <div className="h-3 bg-gray-300 rounded w-24"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentSkeleton;
